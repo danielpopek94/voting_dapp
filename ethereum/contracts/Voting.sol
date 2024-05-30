6f476a7fe57af6c339ba6c3861b9670a0732b7cf
Daniel <henio09@wp.pl>
2024-05-30 13:24:08 +0200

    fix smart contract, delete add candidate function

--- ethereum/contracts/Voting.sol
+++ ethereum/contracts/Voting.sol
@@ -5,34 +5,30 @@ contract Voting {
     struct Option {
         uint id;
         string name;
+				string slogan;
         uint voteCount;
     }
 
     mapping(uint => Option) public options;
     mapping(address => bool) public voters;
-    uint public optionsCount;
     address public owner;
 
     constructor() {
         owner = msg.sender;
-    }
-
-    function addOption(string memory _name) public {
-        require(msg.sender == owner, "Only owner can add options");
-        optionsCount ++;
-        options[optionsCount] = Option(optionsCount, _name, 0);
+        options[1] = Option(1, "Mr Kitty", "Choose me as your next board member. Purr.", 0);
+        options[2] = Option(2, "Mr Doggo", "I'm a good boy. If you choose me, we'll be ruff!", 0);
     }
 
     function vote(uint _optionId) public {
         require(!voters[msg.sender], "You have already voted");
-        require(_optionId > 0 && _optionId <= optionsCount, "Invalid option");
+        require(_optionId == 1 || _optionId == 2, "Invalid option");
 
         voters[msg.sender] = true;
         options[_optionId].voteCount++;
     }
 
     function getOption(uint _optionId) public view returns (Option memory) {
-        require(_optionId > 0 && _optionId <= optionsCount, "Invalid option");
+        require(_optionId == 1 || _optionId == 2, "Invalid option");
         return options[_optionId];
     }
 }
